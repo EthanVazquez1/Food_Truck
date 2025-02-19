936b8dda382694664aef9bf2bcfcfe648738fae3
ethanvazquez <ethanvazquez@CC-JFZC8V3>
2025-02-19 10:51:28 -0800

    Add project files.

---
+++ Food_Truck/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Food_Truck.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
